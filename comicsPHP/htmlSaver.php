acab1d4435c8482f687421f4c1c8e6e84a851416
NicolaZ <nicola.zunino.extern@jsmd-group.com>
2020-04-26 10:29:37 +0200

    Linter and formatter added

--- comicsPHP/htmlSaver.php
+++ comicsPHP/htmlSaver.php
@@ -1,25 +1,21 @@
 <?php
 function saveHTML($remoteURL, $fileName)
 {
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $remoteURL);
-curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-$content = curl_exec($ch);
-if(curl_errno($ch)) {
-    echo curl_error($ch);
-}
-else 
-{
-    $fh = fopen($fileName, 'w');
-    if(!$fh) {
-        echo "Error by creating file";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $remoteURL);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    $content = curl_exec($ch);
+    if (curl_errno($ch)) {
+        echo curl_error($ch);
+    } else {
+        $fh = fopen($fileName, 'w');
+        if (!$fh) {
+            echo "Error by creating file";
+        } else {
+            fwrite($fh, $content);
+            fclose($fh);
+        }
     }
-    else {
-        fwrite($fh, $content);
-        fclose($fh);
-    }
+    curl_close($ch);
 }
-curl_close($ch);
-}
-?>
