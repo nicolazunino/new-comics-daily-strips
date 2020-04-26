061cbe562ef06963ad74b351609eca500d153db1
nicolazunino <64372703+nicolazunino@users.noreply.github.com>
2020-04-26 20:13:59 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <meta name="generator" content="AlterVista - Editor HTML"/>
-  <title>Daily Comics</title>
+  <title>Daily Strip Comics</title>
   <script type="text/javascript" src="comicsJavascript/comicsURL.js"></script>
   <script type="text/javascript">
   document.addEventListener("DOMContentLoaded", function() {
